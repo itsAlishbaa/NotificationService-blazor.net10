25aba1a7fdeb6ed6a0ff7834437907e1caba8187
itsAishbaa <aalishbaa3@gmail.com>
2026-06-07 12:44:29 -0700

    adding code

--- Components/Pages/NotificationConfig.cs
+++ Components/Pages/NotificationConfig.cs
@@ -2,5 +2,7 @@
 {
     public class NotificationConfig
     {
+        public int DefaultNumberOfNotifications { get; set; } = 3; // Default value
+        public string NotificationStyle { get; set; } = "Detailed"; // Default style: "Compact" or "Detailed"
     }
 }
